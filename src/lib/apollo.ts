27e647dbcced5c5e630c70315b7f0df80faf0078
Oseias Saboia <oseiassaboia2@gmail.com>
2022-07-06 16:10:56 -0300

    Conclusão da ultima aula do ignite lab 2022

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri:'https://api-sa-east-1.graphcms.com/v2/cl4pw6zpb2e3001xnfq2tctzu/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`  
+  },
   cache: new InMemoryCache()
 })
